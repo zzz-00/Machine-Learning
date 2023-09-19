193b9049c3777b1b5dd24411ec9f8ef6b70eafbd
zzz-00 <576423404@qq.com>
2023-09-19 19:26:57 +0800

    modify

--- 02Data/main.py
+++ 02Data/main.py
@@ -5,12 +5,13 @@
 # Loading a Dataset
 import torch
 from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
 from torchvision import datasets
 from torchvision.transforms import ToTensor
+from torchvision.io import read_image
 import matplotlib.pyplot as plt
 import os
 import pandas as pd
-from torchvision.io import read_image
 
 
 # Get current working directory path
@@ -64,16 +65,20 @@ plt.show()
 class CustomImageDataset(Dataset):
     def __init__(
         self, annotations_file, img_dir, transform=None, target_transform=None
-    ):
+    ):  # The __init__ function is run once when instantiating the Dataset object.
         self.img_labels = pd.read_csv(annotations_file)
         self.img_dir = img_dir
         self.transform = transform
         self.target_transform = target_transform
 
-    def __len__(self):
+    def __len__(
+        self,
+    ):  # The __len__ function returns the number of samples in our dataset.
         return len(self.img_labels)
 
-    def __getitem__(self, idx):
+    def __getitem__(
+        self, idx
+    ):  # The __getitem__ function loads and returns a sample from the dataset at the given index idx
         img_path = os.path.join(self.img_dir, self.img_labels.iloc[idx, 0])
         image = read_image(img_path)
         label = self.img_labels.iloc[idx, 1]
@@ -82,3 +87,21 @@ class CustomImageDataset(Dataset):
         if self.target_transform:
             label = self.target_transform(label)
         return image, label
+
+
+# Preparing your data for training with DataLoaders
+# While training a model, we typically want to pass samples in “minibatches”, reshuffle the data at every epoch to reduce model overfitting
+# and use Python’s multiprocessing to speed up data retrieval.
+train_dataloader = DataLoader(training_data, batch_size=64, shuffle=True)
+test_dataloader = DataLoader(test_data, batch_size=64, shuffle=True)
+
+# Iterate through the DataLoader
+# Display image and label
+train_features, train_labels = next(iter(train_dataloader))
+print(f"Feature batch shape: {train_features.size()}")
+print(f"Labels batch shape: {train_labels.size()}")
+img = train_features[0].squeeze()
+label = train_labels[0]
+plt.imshow(img, cmap="gray")
+plt.show()
+print(f"Label: {label}")
